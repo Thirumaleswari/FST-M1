f1484956e88c006391ea547f00e9bfe543670924
Thirumaleswari Banugu <thbanugu@in.ibm.com>
2022-05-02 11:05:40 +0530

    uploading activities

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,14 @@
+package activities;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        Car Indiga = new Car();
+        Indiga.make= 2014;
+        Indiga.Color = "Black";
+        Indiga.Transmission = "manual";
+        Indiga.displayCharacteristics();
+        Indiga.accelearate();
+        Indiga.brake();
+
+    }
+}
